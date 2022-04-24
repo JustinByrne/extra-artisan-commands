9226ffd0ce633ed2dd474e9d60a30e2bb2c0340f
Justin Byrne <theskaterjesus@gmail.com>
2022-04-24 18:52:31 +0100

    Added config options for create:user command

--- config/extra-artisan-commands.php
+++ config/extra-artisan-commands.php
@@ -1,5 +1,29 @@
 <?php
-// config for JustinByrne/ExtraArtisanCommands
-return [
 
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Create new user command
+    |--------------------------------------------------------------------------
+    |
+    | Choosing if the create:user command is enabled.
+    |
+    */
+    "is_user" => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Create new user fields
+    |--------------------------------------------------------------------------
+    |
+    | The fields that are needed to create a new user, add or remove as
+    | required.
+    |
+    */
+    "user_fields" => [
+        "name" => "string",
+        "email" => "email",
+        "password" => "password",
+    ],
 ];
