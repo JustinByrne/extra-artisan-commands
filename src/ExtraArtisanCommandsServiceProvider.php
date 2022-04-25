d20dea4374407a37cb4fe99b53f0c35a6a8076b8
Justin Byrne <theskaterjesus@gmail.com>
2022-04-25 20:16:38 +0100

    Added command if config is true

--- src/ExtraArtisanCommandsServiceProvider.php
+++ src/ExtraArtisanCommandsServiceProvider.php
@@ -10,14 +10,13 @@ class ExtraArtisanCommandsServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('extra-artisan-commands')
             ->hasConfigFile()
             ->hasCommand(ExtraArtisanCommandsCommand::class);
+        
+        if (config("extra-artisan-commands.is_user")) {
+            $package->hasCommand(CreateUserCommand::class);
+        }
     }
 }
