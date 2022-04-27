1b18af64ec24e272e898d7a096bf2e7a1c80daa0
Justin Byrne <theskaterjesus@gmail.com>
2022-04-27 20:33:16 +0100

    Created the basis for the user command

--- src/Commands/CreateUserCommand.php
+++ src/Commands/CreateUserCommand.php
@@ -2,7 +2,10 @@
 
 namespace JustinByrne\ExtraArtisanCommands\Commands;
 
+use App\Models\User;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class CreateUserCommand extends Command
 {
@@ -12,7 +15,35 @@ class CreateUserCommand extends Command
 
     public function handle(): int
     {
-        $this->comment('All done');
+        $user = [];
+        $validation = [];
+        
+        foreach (config('extra-artisan-commands.user_fields') as $field => $type) {
+            if ($type == "password") {
+                $user[$field] = Hash::make($this->secret($field));
+                $validation[$field] = ["required"];
+            } else {
+                $user[$field] = $this->ask($field);
+                $validation[$field] = ["required", $type];
+            }
+
+        }
+
+        $validator = Validator::make($user, $validation);
+
+        if ($validator->fails()) {
+            $this->info('User not created. See error messages below:');
+
+            foreach ($validator->errors()->all() as $error) {
+                $this->error($error);
+            }
+
+            return 1;
+        }
+        
+        $user = User::create($user);
+
+        $this->info("User $user->name was created.");
 
         return self::SUCCESS;
     }
